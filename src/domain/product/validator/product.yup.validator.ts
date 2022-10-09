f2a7b43ab1bc1417c0d719637cd7fa629261e768
Vitoriano <vitorianoernandes2012@gmail.com>
2022-10-09 18:38:36 -0300

    Validators para products

---
+++ src/domain/product/validator/product.yup.validator.ts
@@ -0,0 +1,37 @@
+
+import * as yup from "yup";
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import Product from "../entity/product";
+
+export default class ProductYupValidator implements ValidatorInterface<Product> {
+
+  validate(entity: Product): void {
+    try {
+      
+      yup
+      .object()
+      .shape({
+        id: yup.string().required("Id is required"),
+        name: yup.string().required("Name is required")
+      })
+      .validateSync(
+        {
+          id: entity.id,
+          name: entity.name
+        },
+        {
+          abortEarly: false
+        }
+      )
+    } catch (errors) {
+
+      const e = errors as yup.ValidationError;
+      e.errors.forEach((error) => {
+        entity.notification.addError({
+          context: "Customer",
+          message: error,
+        })
+      })
+    }
+  }
+}
